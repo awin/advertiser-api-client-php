6e678aec738379d11165644564f589dc04a362f1
iryna.sribna <iryna.sribna@zanox.com>
2015-03-26 11:37:10 +0100

    Get required arguments and prepare the url

--- Main.php
+++ Main.php
@@ -1,11 +1,56 @@
 <?php
         require 'RestClient.php';
-     
-        // create curl resource
+
+        define('APP_URL', 'https://advertiser.api.zanox.com/advertiser-api/2015-03-01');
+        define('BASE_REST_APP', '/report/program/');
+        define('ARGUMENTS_NUMBER', '6');
+
+        if (sizeof($argv) != ARGUMENTS_NUMBER) {
+            echo 'Wrong number of arguments. Correct usage: Main.php --[header|url] PROGRAM_ID CONNECT_ID SECRET_KEY GROUP_BY"';
+            exit;
+        }
+
+        $auth_type = $argv[1];
+        $programId = $argv[2];
+        $connectId = $argv[3];
+        $secretKey = $argv[4];
+        $groupBy = $argv[5];
+
+        if (!isGroupByValid($groupBy)) {
+            $groupBy = 'day';
+            echo 'Group by parameter invalid, default group by: day will be used.';
+        }
+
+        $c = new RestClient();
+//        $c->setConnectId($connectId);
+//        $c->setSecretKey($secretKey);
+        $nonce = $c->getNonce();
+        $timestamp = gmdate('D, d M Y H:i:s T');
+        $sign = $c->getSignature('GET', '/report/program/' . $programId, $nonce, $timestamp);
+        $d = strtotime("-31 days");
+        $fromdate = date("Y-m-d", $d);
+        $d = strtotime("yesterday");
+        $todate = date("Y-m-d", $d);
+
         $ch = curl_init();
 
+        $parameter = array(
+           'groupby'=>$groupBy,
+           'fromdate'=>'2015-03-01',
+           'todate'=>$todate,
+           'connectid'=>$connectId,
+           'date'=>$timestamp,
+           'nonce'=>$nonce,
+           'signature'=>$sign);
+
+        if ( is_array($parameter) ) {
+            $parameterQuery = http_build_query($parameter);
+        }
+
         // set url
-        curl_setopt($ch, CURLOPT_URL, "https://api.zanox.com/json/2011-03-01/products?connectid=43EEF0445509C7205827&items=5&q=nike");
+        $url = APP_URL . BASE_REST_APP . $programId . '?' . $parameterQuery;
+        echo $url;
+        curl_setopt($ch, CURLOPT_URL, $url);
 
         //return the transfer as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -14,16 +59,16 @@
         $output = curl_exec($ch);
 
         echo $output;
+
         // close curl resource to free up system resources
-        curl_close($ch);     
+        curl_close($ch);
 
-       $c = new RestClient();
-       $nonce = $c->getNonce();
-       $date = gmdate('D, d M Y H:i:s T');
-       //$sign = $c->getSignature('GET', $uri, $nonce);
-
-       echo $nonce;
-       echo $date;
-       //echo $sign;
+        function isGroupByValid($groupBy) {
+            $validGroupByArray = array('day', 'month', 'adspace', 'admedium', 'adspace,admedium', 'admedium,adspace');
+            if (in_array($groupBy, $validGroupByArray)) {
+                return true;
+            }
+            return false;
+       }
 
 ?>
