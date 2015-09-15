0994b02938656840b2874bc7b8b17c7b589948b7
Michael Renz <michael.renz@zanox.com>
2015-09-15 12:50:11 +0200

    ZPM-8628, added example for partner code service

---
+++ src/AdvertiserApiClient.php
@@ -0,0 +1,26 @@
+<?php
+		require './src/AuthenticationUtil.php';
+    require './src/ReportService.php';
+    require './src/PartnerCodeService.php';
+
+    define('APP_URL', 'https://advertiser.api.zanox.com/advertiser-api/2015-03-01');
+    
+    if (sizeof($argv) < 2) {
+	        error_log('Wrong number of arguments.');
+	        exit(1);
+	  }
+	  
+	  $serviceType = $argv[1];
+	  switch($serviceType) {
+	  	case "reportservice":
+	  		$reportService = new ReportService(array_splice($argv, 2, count($argv)));
+    		echo $reportService->getReportData();
+        break;
+    	case "partnercodeservice":
+		    $partnerCodeService = new PartnerCodeService(array_splice($argv, 2, count($argv)));
+    		echo $partnerCodeService->getPartnerCodeData();
+        break;
+     default:
+       echo "No service found for type:" . $serviceType . ", use [reportservice|partnercodeservice] only." ;     
+		}
+        
